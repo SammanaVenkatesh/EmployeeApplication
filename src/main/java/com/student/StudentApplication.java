1f56b5f97ab768e5924e9d2a715e6bf1dbd8bc52
SammanaVenkatesh <sammana2.java@gmail.com>
2025-03-02 23:07:41 +0530

    adding first comments

--- src/main/java/com/student/StudentApplication.java
+++ src/main/java/com/student/StudentApplication.java
@@ -3,6 +3,7 @@ package com.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//adding comment
 @SpringBootApplication
 public class StudentApplication {
 
