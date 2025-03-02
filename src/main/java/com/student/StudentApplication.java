9c763231dc4ee96d39851d010fbdc00f98df5938
SammanaVenkatesh <sammana2.java@gmail.com>
2025-03-02 23:12:10 +0530

    delete

--- src/main/java/com/student/StudentApplication.java
+++ src/main/java/com/student/StudentApplication.java
@@ -3,7 +3,6 @@ package com.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//adding comment
 @SpringBootApplication
 public class StudentApplication {
 
